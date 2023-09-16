c59f881ccacf53fe55387d9f564be83ab88fe20b
Lance Holsten <58127678+buggily@users.noreply.github.com>
2023-09-16 00:43:42 -0500

    clean

--- app/src/main/java/com/buggily/skeleton/ui/SkeletonApplication.kt
+++ app/src/main/java/com/buggily/skeleton/ui/SkeletonApplication.kt
@@ -1,7 +1,15 @@
 package com.buggily.skeleton.ui
 
 import android.app.Application
+import com.google.android.material.color.DynamicColors
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class SkeletonApplication : Application()
+class SkeletonApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+}
--- app/src/main/java/com/buggily/skeleton/ui/main/MainActivity.kt
+++ app/src/main/java/com/buggily/skeleton/ui/main/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.buggily.skeleton.ui.main
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsControllerCompat
@@ -20,28 +22,47 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         installSplashScreen()
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setupWindow(Build.VERSION.SDK_INT < Build.VERSION_CODES.O)
+        setupNavigationController()
+    }
+
+    private fun setupWindow(decorFitsSystemWindows: Boolean) {
         WindowCompat.setDecorFitsSystemWindows(
             window,
-            false
+            decorFitsSystemWindows
         )
+
+        val systemBarsColor: Int = if (decorFitsSystemWindows) {
+            R.color.system_bars_translucent
+        } else {
+            R.color.system_bars_transparent
+        }.let { ContextCompat.getColor(this, it) }
+
+        window.statusBarColor = systemBarsColor
+        window.navigationBarColor = systemBarsColor
+
+        if (decorFitsSystemWindows) {
+            return
+        }
+
+        val isDark: Boolean = resources.getBoolean(R.bool.is_dark)
+        val isLight: Boolean = !isDark
 
         val insetsController = WindowInsetsControllerCompat(
             window,
             window.decorView
         )
 
-        val isDark: Boolean = resources.getBoolean(R.bool.is_dark)
-        val isLight: Boolean = !isDark
-
         with(insetsController) {
             isAppearanceLightStatusBars = isLight
             isAppearanceLightNavigationBars = isLight
         }
+    }
 
+    private fun setupNavigationController() {
         val navHostFragment: NavHostFragment = supportFragmentManager.findFragmentById(
             R.id.activity_main_nav_host_fragment
         ) as NavHostFragment
